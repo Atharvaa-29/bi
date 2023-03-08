3687c53adc4ce6550317d14c8ecae43c0ef346cc
Atharvaa-29 <97778548+Atharvaa-29@users.noreply.github.com>
2023-03-08 21:49:15 +0530

    Add files via upload

---
+++ 7b decision tree.R
@@ -0,0 +1,9 @@
+# dependent packages.
+packages.install("party")
+library(party)
+print(head(readingSkills))
+input.dat= readingSkills[c(1:105),]
+output.tree =ctree(nativeSpeaker ~ age + shoeSize + score, data = input.dat)
+plot(output.tree,)
+
+
